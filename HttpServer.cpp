4f44712a2bcae5f7a05a3cd10e391f4ae4be8205
k0ch <k0ch@riseup.net>
2017-07-28 11:49:48 -0300

    Feature to return page files (from executable folder)

--- HttpServer.cpp
+++ HttpServer.cpp
@@ -1,11 +1,22 @@
+//STD
 #include <memory>
+#include <string>
+#include <fstream>
+
+//LIBEVENT
 #include <evhttp.h>
+
+const char* M404 = "<html><body>This page doesn't exist (404)</body></html>";
 
 int main()
 {
     if (!event_init()) return -1;
 
-    std::unique_ptr<evhttp, decltype(&evhttp_free)> EventHttp(evhttp_start("127.0.0.1", 8081), &evhttp_free);
+    std::unique_ptr<evhttp, decltype(&evhttp_free)> EventHttp(
+        evhttp_start("127.0.0.1", 8081),
+        &evhttp_free
+    );
+
     if (!EventHttp) return -1;
 
     auto GenericReq = [](evhttp_request* req, void*)
@@ -13,8 +24,34 @@ int main()
         auto OutBuf = evhttp_request_get_output_buffer(req);
         if (!OutBuf) return;
 
-        evbuffer_add_printf(OutBuf, "<html><body><h1>Hello World!</h1></body></html>");
-        evhttp_send_reply(req, HTTP_OK, "", OutBuf);
+        std::string uri = evhttp_request_get_uri(req);
+
+        if (*(uri.end()-1) == '/')
+            uri = uri + "index.html";
+        else if (uri.rfind(".") == std::string::npos)
+            uri = uri + ".html";
+
+        //erase first '/'
+        uri.erase(0,1);
+
+        std::string page;
+        int code;
+
+        //get page reading requested file
+        std::ifstream infile(uri);
+        if(infile.is_open()) {
+            page.assign(
+                (std::istreambuf_iterator<char>(infile)),
+                std::istreambuf_iterator<char>()
+            );
+            code = HTTP_OK;
+        } else {
+            page = M404;
+            code = HTTP_NOTFOUND;
+        }
+
+        evbuffer_add_printf(OutBuf, "%s", page.c_str());
+        evhttp_send_reply(req, code, "", OutBuf);
     };
 
     evhttp_set_gencb(EventHttp.get(), GenericReq, nullptr);
